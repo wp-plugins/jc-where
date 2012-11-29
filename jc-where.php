61fa1ce8c238f5f0b2b5775f857913cde439109e
bibichette <bibichette@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-29 04:07:33 +0000

    change author information
    git-svn-id: https://plugins.svn.wordpress.org/jc-where/trunk@631579 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jc-where.php
+++ jc-where.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
 /*
 Plugin Name: JC Where
-Plugin URI: http://www.madeby-jc.com/portfolio/jc-where/
+Plugin URI: http://www.netcod.es/
 Description: Display the IP of the server in the administration footer
-Version: 1.0
-Author: Julien Chanseaume
-Author URI: http://www.madeby-jc.com/
+Version: 1.1
+Author: Netcodes
+Author URI: http://www.netcod.es/
  */
 
 function jcserver_display(){
     echo '<p id="footer-jc-where" class="alignright" style="margin-left: 10px;"><strong title="Server IP Address">['.$_SERVER['SERVER_ADDR'].']</strong></p>';
 }
- 
-add_action('in_admin_footer', 'jcserver_display'); 
+
+add_action('in_admin_footer', 'jcserver_display');
 
 ?>
